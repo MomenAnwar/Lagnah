6e16830bbf96b39fb6a3b937bc11f069d12c6dc4
momen-anwar <anwrmwmn68@gmail.com>
2025-04-20 15:11:27 +0200

    welcome

--- frontend/src/components/Home/Main.jsx
+++ frontend/src/components/Home/Main.jsx
@@ -11,7 +11,11 @@ const Main = () => {
       setSelectedNavbar('main')
     }, [setSelectedNavbar])
   return (
-    <div>Main</div>
+    <div className='flex flex-col items-center justify-center bg-gray-100'>
+      <h1 className='text-3xl font-bold text-center mt-10 mb-5'>
+        مرحبا بكم فى موقع لجنة الزكاة والصدقات ببهنيا
+      </h1>
+    </div>
   )
 }
 
